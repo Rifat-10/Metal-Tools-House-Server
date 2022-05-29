aef0bbcac6589ec266fd3b325e946a87dedd6854
Rifat-10 <mehedihassanrifat.3@gmail.com>
2022-05-29 14:09:36 +0600

    Tools Data Add

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+require("dotenv").config();
 const express = require('express');
-const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+
+
+const app = express();
 
 app.get('/', (req, res) => {
     res.send('Look MaMa!')
-  });
+});
 
-  
-  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jszmv.mongodb.net/?retryWrites=true&w=majority`;
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-  
-  
 
-  app.listen(port, () => {
-      console.log('Listening to port', port);
-  })
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jszmv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db("metalToolsHouse").collection("tools");
+
+        app.get('/tools', async(req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        })
+
+    }
+    finally {
+        //   await client.close();  
+    }
+}
+
+run().catch(console.dir);
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
